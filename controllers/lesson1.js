78091bf490b6ee0f25ce57abb82150f2f7312657
Kathy Moore <mookathy3@gmail.com>
2023-04-22 23:21:53 -0400

    going above and beyond for 100% on first assignment :)

--- controllers/lesson1.js
+++ controllers/lesson1.js
@@ -2,11 +2,16 @@ const jessicaRoute = (req,res) => {
     res.send("Jessica Moore");
   };
 
+  const chandlerRoute = (req,res) => {
+    res.send("Chandler Moore");
+  };
+
 const elyseRoute = (req,res) => {
     res.send("Elyse Moore");
   };
 
 module.exports = {
     jessicaRoute,
+    chandlerRoute,
     elyseRoute
 };
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
 
 routes.get('/', lesson1Controller.jessicaRoute);
+routes.get('/chandler', lesson1Controller.chandlerRoute);
 routes.get('/elyse', lesson1Controller.elyseRoute);
 
 module.exports = routes;
